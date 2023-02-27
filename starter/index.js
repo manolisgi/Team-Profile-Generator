e6f6b0f742562088cd839bc771e0cc3416cea67e
manolisgi <114882721+manolisgi@users.noreply.github.com>
2023-02-27 22:25:24 +0000

    added the emploee selection

--- starter/index.js
+++ starter/index.js
@@ -1,15 +1,240 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
+const renderTeam = require('./src/page-template');
 
-const render = require("./src/page-template.js");
+teamArray = [];
 
+function run() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: "Please choose your employee's role!",
+        name: 'role',
+        choices: ['Manager', 'Engineer', 'Intern', 'No More Members'],
+      },
+    ]).then(function (inputs) {
+      if (inputs.role === 'Manager') {
+        managerQuestion();
+      }
+      else if (inputs.role === 'Engineer') {
+        engineerQuestion();
+      }
+      else if (inputs.role === 'Intern') {
+        internQuestion();
+      }
+      else if (inputs.role === 'No More Members') {
+        createTeam();
+      }
+    })
+}
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+function managerQuestion() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'What is the name of the teams manager?',
+      name: 'mName',
+      validate: (mName) => {
+        if (mName) {
+          return true;
+        }
+        else {
+          console.log('Please enter name of the teams manager!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the manager's ID!",
+      name: 'mID',
+      validate: (mID) => {
+        if (mID) {
+          return true;
+        }
+        else {
+          console.log("Please enter the manager's ID!");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the manager's email!",
+      name: 'mEmail',
+      validate: (mEmail) => {
+        if (mEmail) {
+          return true;
+        }
+        else {
+          console.log("Please enter the manager's email!");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the manager's office number!",
+      name: 'mOfficeNumber',
+      validate: (mOfficeNumber) => {
+        if (mOfficeNumber) {
+          return true;
+        }
+        else {
+          console.log("Please enter the manager's office number");
+          return false;
+        }
+      }
+    },
+  ]).then((answers) => {
+    const Manager = new manager(answers.mName, answers.mID, answers.mEmail, answers.mOfficeNumber);
+    teamArray.push(Manager);
+    run();
+  });
+}
 
+function engineerQuestion() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Please enter the name of the Engineer!',
+      name: 'eName',
+      validate: (eName) => {
+        if (eName) {
+          return true;
+        }
+        else {
+          console.log('Please enter the name of the Engineer!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the Engineer's ID!",
+      name: 'eID',
+      validate: (eID) => {
+        if (eID) {
+          return true;
+        }
+        else {
+          console.log("Please enter the Engineer's ID");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the Engineer's email!",
+      name: 'eEmail',
+      validate: (eEmail) => {
+        if (eEmail) {
+          return true;
+        }
+        else {
+          console.log("Please enter the Engineer's email");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the Engineer's github username!",
+      name: 'github',
+      validate: (github) => {
+        if (github) {
+          return true;
+        }
+        else {
+          console.log("Please enter the Engineer's github username");
+          return false;
+        }
+      }
+    },
+  ]).then((answers) => {
+    const Engineer = new engineer(answers.eName, answers.eID, answers.eEmail, answers.github);
+    teamArray.push(Engineer);
+    run();
+  });
+}
+
+function internQuestion() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Please enter the name of the Intern!',
+      name: 'iName',
+      validate: (iName) => {
+        if (iName) {
+          return true;
+        }
+        else {
+          console.log('Please enter the name of the Intern!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the Intern's ID!",
+      name: 'iID',
+      validate: (iID) => {
+        if (iID) {
+          return true;
+        }
+        else {
+          console.log("Please enter the Intern's ID");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the Intern's email!",
+      name: 'iEmail',
+      validate: (iEmail) => {
+        if (iEmail) {
+          return true;
+        }
+        else {
+          console.log("Please enter the Intern's email");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      message: "Please enter the Intern's school username!",
+      name: 'school',
+      validate: (school) => {
+        if (school) {
+          return true;
+        }
+        else {
+          console.log("Please enter the Intern's school username");
+          return false;
+        }
+      }
+    },
+  ]).then((answers) => {
+    const Intern = new intern(answers.iName, answers.iID, answers.iEmail, answers.school);
+    teamArray.push(Intern);
+    run();
+  });
+}
+
+function createTeam() {
+  console.log("Team Proflies created success!")
+  fs.writeFile('./output/team.html', renderTeam(teamArray), function (err) {
+    if (err) {
+      return console.log(err)
+    }
+  });
+}
+
+run();
--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,1 +1,18 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(officeNumber, name, id, email) {
+        super(name, id, email)
+        this.officeNumber = officeNumber;
+    }
+        getOfficeNumber() {
+            return this.officeNumber;
+        }
+
+        getRole() {
+            return "Manager";
+        }
+
+}
+
+module.exports = Manager;
